0d08417e1084b7a2ac41aa4c9cc3f3672f6cd403
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-05-02 19:56:07 -0500

    set up Asset folder in src

--- newpawtastic/src/App.js
+++ newpawtastic/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component() {
     constructor(props){
       super(props)
+      
+
     }
-  }
-  render {
+  
+  render() {
+  
     return (
       <div>
         
       </div>
     );
   }
+}
 
-
-  }
+  
   
 
 export default App;
